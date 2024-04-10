f8a1b300cc62793d6a08f818c1e9c45e8f9b95db
BrightTechUp <bright@techupth.com>
2024-04-10 16:10:46 +0700

    remove initial code

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,1 @@
-// Exercise #1: For Each Function
-
-function forEach(array, operation) {
-  // Start coding here
-}
-
-const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
-
-// Using `forEach` function here
-
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+// Start coding here
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,8 @@
-//Exercise #2: At Least Five Function
-
-function atLeastFive(array, operation) {
-  // Start coding here
-}
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
+// Start coding here
 
-// Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+
+
