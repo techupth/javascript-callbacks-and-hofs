b456a7874a3d6704e4999fe83c9e47dd4780e25f
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-05-17 15:59:58 +0700

    feat : callback funtion

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+// employeeSalaries.forEach((salaries) => console.log(salaries));
+const forEach = (array,callback) => {
+  for (let i = 0; i < array.length;i++ ){
+    let salary = array[i];
+    newEmployeeSalaries.push(callback(salary));
+  }
+}
+function plus(num) {
+  return num + 5000;
+}
+forEach(employeeSalaries,plus)
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries); 
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,30 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
-}
+    let total =0;
+    for(let i=0 ; i < array.length ; i++ ){
+      let scorenumber = array[i];
+      if(operation(scorenumber)){
+        total++
+      }
+    }
+    return total >=5;
+  }
+
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+// atLeastFive(scoreRoom1Result,operation)
+function passScore (score){
+  return score>70;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,passScore)? "ผ่านเกณฑ์ ✅": "ไม่ผ่านเกณฑ์ ❌";
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,passScore)? "ผ่านเกณฑ์ ✅": "ไม่ผ่านเกณฑ์ ❌";
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,passScore)? "ผ่านเกณฑ์ ✅": "ไม่ผ่านเกณฑ์ ❌";
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
