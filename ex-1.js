f5ce513c8d7d40e1de1a28760cf9396df586026e
Thanapat1502 <psy150201027@gmail.com>
2024-10-08 14:26:18 +0700

    feat: add function to ex1-ex2

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,18 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let eachSalaries of array) {
+    newEmployeeSalaries.push(operation(eachSalaries));
+  }
 }
+const salaryRaise = (eachSalaries) => {
+  return eachSalaries + 5000;
+};
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, salaryRaise);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,41 @@
 //Exercise #2: At Least Five Function
+/**ต่อมาโรงเรียนมีเกณฑ์ในการตรวจสอบคุณภาพของนักเรียนแต่ละห้อง ดังนี้
+
+ถ้ามีนักเรียนจำนวนตั้งแต่ 5 คนขึ้นไป ที่ได้คะแนนมากกว่า 70 จะถือว่าผ่านเกณฑ์ ✅
+ถ้ามีนักเรียนจำนวนไม่ถึง 5 คน ที่ได้คะแนนมากกว่า 70 จะถือว่าไม่ผ่านเกณฑ์ ❌ */
 
 function atLeastFive(array, operation) {
+  //เอาเช็คเกอร์มาเช็คกับเงื่ีอนไข
   // Start coding here
+  const checker = operation(array);
+  console.log(checker);
+  if (checker >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else if (checker < 5) {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
 }
+
+const pass70Checker = (array) => {
+  let checker = 0;
+  for (let score of array) {
+    console.log(score);
+    if (score > 70) {
+      checker++;
+    }
+  }
+  return checker;
+};
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
-
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+pass70Checker(studentScoresRoom1);
+// // Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, pass70Checker);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, pass70Checker);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, pass70Checker);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
